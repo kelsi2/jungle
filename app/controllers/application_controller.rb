66b259f932e231e84adb04765425da8bec760e3a
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-10-14 00:40:29 +0000

    add authentication to admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  def authenticate
+    http_basic_authenticate_with do |username, password|
+      username: ENV["USER_ID"] && password: ENV["PASSWORD"]
+    end
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
