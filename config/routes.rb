c33c50647e96a68fe2696059a2c48e5bb205929e
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-10-14 18:18:38 +0000

    add about route and link in nav

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,23 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
-
+  
+  resources :about, only: [:show]
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
