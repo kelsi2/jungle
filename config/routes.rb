435ab0349c41af7d77ad07409a075608ed02eef6
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-10-15 23:08:20 +0000

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,10 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
-
   end
+
+  resources :users, only:[:create, :new]
+  resources :sessions, only: [:create, :new, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
