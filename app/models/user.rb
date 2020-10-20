4fb3af0e9a9767322000a71a9967ef4a1953f18e
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-10-20 00:40:26 +0000

    Add first_name and last_name to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  validates :password, presence: true
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
+  validates :email, presence: true
+  validates_uniqueness_of :email
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+      it 'should save if provided with a password and matching password confirmation' do
+        @user = User.new(
+          first_name: "Kelsi",
+          last_name: "Proulx",
+          email: "kelsiproulx1919@gmail.com",
+          password: "testMe1234",
+          password_confirmation: "testMe1234"
+        )
+        expect(@user).to be_valid
+      end
+
+      it 'should not save if password and password confirmation do not match' do
+        @user = User.new(
+          first_name: "Kelsi",
+          last_name: "Proulx",
+          email: "kelsiproulx1919@gmail.com",
+          password: "testMe1234",
+          password_confirmation: "testMe123"
+        )
+        expect(@user).to_not be_valid
+      end
+
+      it 'should not save if missing either password or password confirmation' do
+      end
+
+      it 'should not save if password is less than 8 characters' do
+      end
+
+      it 'should not save if no email is provided' do
+      end
+
+      it 'should not save if no first name is provided' do
+      end
+
+      it 'should not save if no last name is provided' do
+      end
+
+      it 'should disregard case sensitivity in email' do
+      end
+    end
+
+  describe '.authenticate_with_credentials' do
+    it 'should not save if user name already exists' do
+    end
+
+    it 'should return the user if the email is registerd' do
+    end
+
+    it 'should not return the user if the email is not registered' do
+    end
+
+    it 'should still return the user if email includes leading or trailing whitespace' do
+    end
+  end
 end
